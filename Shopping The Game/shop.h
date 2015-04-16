69da897dd26650acb68e5dee39d5f4e123e412ca
Firstdragon <anden777@ya.ru>
2015-04-16 09:27:34 +0300

    0.1

--- Shopping The Game/shop.h
+++ Shopping The Game/shop.h
@@ -7,39 +7,25 @@
 #include <windows.h>
 #include <conio.h>
 #include <ctime>
+#include "Mapcreator.h"
+#include "enum.h"
 
 using namespace 
 			std;
 
 class shop
 {
-private:
-	enum class cell { BLANK, WALL, PLAYER1, PLAYER2, BONUS };
+public:
 	int len;
 	int wid;
 	cell **floor;
-	unsigned short moveKeys;
-	int happiness_p1;
-	int happiness_p2;
-	int bonusCount;
-public:
+	//Конструктор по умолчанию
+	shop();
+
 	//Конструктор карты с заданным размером
 	shop (int g, int h);
 
 	//Деструктор
 	~shop ();
-
-	//Функция, рисующая линию
-	//                  коорд. i  | коорд. j |  длина линии |  направление    |    линия чего 
-	void makeLine(int iFrom, int jFrom, int lineLength, string lineWhere, cell lineOfWhat);
-
-	//Функция, проверяющая возможность перемещения
-	int move(cell r);
-
-	//Функция, размещающая бонус в случайном месте карты
-	void spawnBonus ();
-
-	//Вывод
-	void f_out();
 }; 
 
